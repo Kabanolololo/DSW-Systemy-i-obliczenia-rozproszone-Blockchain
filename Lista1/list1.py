88fc7c28553fe05c85cde7f91f2a29f1ed8bd50b
Paweł Muszyński <p.muszynski200@gmail.com>
2025-10-10 12:55:03 +0200

    list1 - zad 6

--- Lista1/list1.py
+++ Lista1/list1.py
@@ -36,6 +36,7 @@ class Block:
 class Blockchain:
     def __init__(self):
         self.blocks = []
+        self.pending_transactions = []
 
     # Metoda do tworzenia bloku poczatkowego
     def add_genesis_block(self):
@@ -47,16 +48,22 @@ class Blockchain:
         return self.blocks[-1]
 
     # Metoda do dodawnia bloku
-    def add_block(self, data, difficulty = 8):
+    def add_block(self, difficulty = 3):
         previous_block = self.get_previous_block()
 
         new_index = previous_block.index + 1
         new_date = str(datetime.datetime.now().timestamp())
         previous_hash = previous_block.hash
 
-        block = Block(new_index, new_date, data, previous_hash)
+        # Dodanie nowego bloku
+        block = Block(new_index, new_date,self.pending_transactions,  previous_hash)
+        
+        # Kopanie bloku
         block.mine_block(difficulty)
         self.blocks.append(block)
+        
+        # Czystka buforu
+        self.pending_transactions = []
 
     # Metoda do walidacji hashy
     def is_chain_valid(self):
@@ -65,23 +72,57 @@ class Blockchain:
             previous_block_hash = self.blocks[x - 1].hash
 
             if previous_hash != previous_block_hash:
-                print(f"Blok {x} ma invalid hash poprzedniego bloku!")
+                print(f"Blok {x} ma niepoprawny hash poprzedniego bloku!")
                 return False
 
         print("Hashes are valid.")
         return True
-
+    
+    # Metoda dodawania transakcji
+    def add_transaction(self, sender, receiver, amount):
+        transaction = {
+            "sender": sender,
+            "receiver": receiver,
+            "amount": amount
+        }
+        self.pending_transactions.append(transaction)
 
 # Inicjalizacja blockchaina
 my_blockchain = Blockchain()
 
-# Dodanie bloku początkowego
+# Dodanie bloku początkowego (genesis block)
 my_blockchain.add_genesis_block()
 
-# Dodanie przykładowych bloków z danymi
-my_blockchain.add_block("Transakcja: Alicja wysyła 10 BTC do Boba")
-my_blockchain.add_block("Transakcja: Bob wysyła 5 BTC do Ewy")
-my_blockchain.add_block("Transakcja: Ewa wysyła 2 BTC do Alicji")
+########################### PRZYKLADOWE BLOKI I TRAKNSAKCJE WYGENEROWANE PRZEZ CHATGPT ###########################
+# 🔹 BLOK 1
+my_blockchain.add_transaction("Alicja", "Bob", 10)
+my_blockchain.add_transaction("Bob", "Ewa", 5)
+my_blockchain.add_transaction("Ewa", "Alicja", 2)
+my_blockchain.add_transaction("Karol", "Marta", 3)
+my_blockchain.add_transaction("Marta", "Tomek", 1)
+my_blockchain.add_block()  # Tworzymy blok z powyższymi transakcjami
+
+# 🔹 BLOK 2
+my_blockchain.add_transaction("Tomek", "Alicja", 4)
+my_blockchain.add_transaction("Ewa", "Karol", 2)
+my_blockchain.add_transaction("Alicja", "Ewa", 6)
+my_blockchain.add_transaction("Marta", "Bob", 5)
+my_blockchain.add_block()  # Drugi blok
+
+# 🔹 BLOK 3
+my_blockchain.add_transaction("Bob", "Karol", 7)
+my_blockchain.add_transaction("Karol", "Ewa", 1)
+my_blockchain.add_transaction("Ewa", "Alicja", 3)
+my_blockchain.add_transaction("Tomek", "Marta", 8)
+my_blockchain.add_transaction("Alicja", "Tomek", 2)
+my_blockchain.add_block()  # Trzeci blok
+
+# 🔹 BLOK 4
+my_blockchain.add_transaction("Marta", "Ewa", 10)
+my_blockchain.add_transaction("Ewa", "Karol", 2)
+my_blockchain.add_transaction("Tomek", "Bob", 1)
+my_blockchain.add_transaction("Alicja", "Marta", 7)
+my_blockchain.add_block()  # Czwarty blok
 
 # Wypisanie bloków i ich hashy
 for block in my_blockchain.blocks:
@@ -90,7 +131,7 @@ for block in my_blockchain.blocks:
     print(f"Data: {block.data}")
     print(f"Previous Hash: {block.previous_hash}")
     print(f"Hash: {block.hash}")
-    print("-" * 30)
+    print("-" * 60)
 
 # Walidacja bloków
 my_blockchain.is_chain_valid()
